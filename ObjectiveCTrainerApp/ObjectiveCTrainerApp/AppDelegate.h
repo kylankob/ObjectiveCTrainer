4b8fc80aa8abd8e2aa2d918faad4efc54fddb9ee
Kylan Koblitz <kylankob@gmail.com>
2014-04-02 06:06:47 -0400

    Initial Commit
    Initial project commit.

---
+++ ObjectiveCTrainerApp/ObjectiveCTrainerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjectiveCTrainerApp
+//
+//  Created by Kylan Koblitz on 4/2/14.
+//  Copyright (c) 2014 Q New Media Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectiveCTrainerApp/ObjectiveCTrainerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectiveCTrainerApp
+//
+//  Created by Kylan Koblitz on 4/2/14.
+//  Copyright (c) 2014 Q New Media Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
